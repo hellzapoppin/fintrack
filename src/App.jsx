4f398f3cf367b922dc6c0f633a876b559c64a270
José Leonardo <joseleonaordo85@gnail.com>
2025-06-06 13:31:37 -0300

    feat: update shadcn theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Button } from './components/ui/button'
 
 const App = () => {
-  return <Button>Hello</Button>
+  return <Button className="bg-primary-blue">Hello</Button>
 }
 
 export default App
