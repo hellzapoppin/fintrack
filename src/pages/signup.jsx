8b45fdbed26e343b9612bc8da809de1b58422e84
José Leonardo <joseleonaordo85@gnail.com>
2025-06-09 19:06:10 -0300

    feat: add api integration on signup

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import PasswordInput from '@/components/password-input'
@@ -23,6 +26,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { api } from '@/lib/axios'
 
 const signupSchema = z.object({
   firstName: z.string().trim().min(1, { message: 'O nome é obrigatório' }),
@@ -45,6 +49,19 @@ const signupSchema = z.object({
 })
 
 const SignupPage = () => {
+  const [user, setUser] = useState(null)
+  const signupMutation = useMutation({
+    mutationKey: ['signup'],
+    mutationFn: async (variables) => {
+      const response = await api.post('/users', {
+        first_name: variables.firstName,
+        last_name: variables.lastName,
+        email: variables.email,
+        password: variables.password,
+      })
+      return response.data
+    },
+  })
   const form = useForm({
     resolver: zodResolver(signupSchema),
     defaultValues: {
@@ -58,7 +75,22 @@ const SignupPage = () => {
   })
 
   const handleSubmit = (data) => {
-    console.log(data)
+    signupMutation.mutate(data, {
+      onSuccess: (createdUser) => {
+        const accessToken = createdUser.tokens.accessToken
+        const refreshToken = createdUser.tokens.refreshToken
+        localStorage.setItem('accessToken', accessToken)
+        localStorage.setItem('refreshToken', refreshToken)
+        setUser(createdUser)
+        toast.success('Conta criada com sucesso!')
+      },
+      onError: () => {
+        toast.error('Erro ao criar conta. Por favor, tente novamente!')
+      },
+    })
+  }
+  if (user) {
+    return <h1>Olá, {user.first_name}</h1>
   }
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
