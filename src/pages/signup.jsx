fbdf12e5ff615e9aeaa0eccd54b035ed287fe4c3
José Leonardo <joseleonaordo85@gnail.com>
2025-06-09 19:19:29 -0300

    feat: ensure password and password confirmation are the same on signup form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -28,25 +28,33 @@ import {
 import { Input } from '@/components/ui/input'
 import { api } from '@/lib/axios'
 
-const signupSchema = z.object({
-  firstName: z.string().trim().min(1, { message: 'O nome é obrigatório' }),
-  lastName: z.string().trim().min(1, { message: 'O sobrenome é obrigatório' }),
-  email: z
-    .string()
-    .email({ message: 'O e-mail é inválido' })
-    .trim()
-    .min(1, { message: 'O e-mail é obrigatório' }),
-  password: z
-    .string()
-    .trim()
-    .min(6, { message: 'A senha deve ter no mínimo 6 caracteres' }),
-  passwordConfirmation: z.string().trim().min(6, {
-    message: 'A confirmação de senha deve ter no mínimo 6 caracteres',
-  }),
-  terms: z.boolean().refine((value) => value === true, {
-    message: 'Você precisa aceitar os termos',
-  }),
-})
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, { message: 'O nome é obrigatório' }),
+    lastName: z
+      .string()
+      .trim()
+      .min(1, { message: 'O sobrenome é obrigatório' }),
+    email: z
+      .string()
+      .email({ message: 'O e-mail é inválido' })
+      .trim()
+      .min(1, { message: 'O e-mail é obrigatório' }),
+    password: z
+      .string()
+      .trim()
+      .min(6, { message: 'A senha deve ter no mínimo 6 caracteres' }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: 'A confirmação de senha deve ter no mínimo 6 caracteres',
+    }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Você precisa aceitar os termos',
+    }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'As senha não coincidem',
+    path: ['passwordConfirmation'],
+  })
 
 const SignupPage = () => {
   const [user, setUser] = useState(null)
