f928a2b0cac85c74c94e83fd47b49d94b1d767e1
José Leonardo <joseleonaordo85@gnail.com>
2025-06-12 13:36:49 -0300

    feat: ensure date range can be selected on home page

--- src/components/ui/date-picker-with-range.jsx
+++ src/components/ui/date-picker-with-range.jsx
@@ -1,4 +1,5 @@
 import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { CalendarIcon } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
@@ -11,30 +12,48 @@ const DatePickerWithRange = ({
   value,
   onChange,
   placeholder = 'Selecione uma data',
+  className,
 }) => {
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          className={cn(
-            'w-[280px] justify-start text-left font-normal',
-            !value && 'text-muted-foreground',
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {value ? format(value, 'PPP') : <span>{placeholder}</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={value}
-          onSelect={onChange}
-          initialFocus
-        ></Calendar>
-      </PopoverContent>
-    </Popover>
+    <div className={cn('grid gap-2', className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant="outline"
+            className={cn(
+              'w-full justify-start text-left font-normal',
+              !value && 'text-muted-foreground',
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {value?.from ? (
+              value.to ? (
+                <>
+                  {format(value.from, 'LLL dd, y', { locale: ptBR })} -{' '}
+                  {format(value.to, 'LLL dd, y', { locale: ptBR })}
+                </>
+              ) : (
+                format(value.from, 'LLL dd y', { locale: ptBR })
+              )
+            ) : (
+              <span>{placeholder}</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="range"
+            defaultMonth={value?.from}
+            selected={value}
+            onSelect={onChange}
+            initialFocus
+            numberOfMonths={2}
+            locale={ptBR}
+          ></Calendar>
+        </PopoverContent>
+      </Popover>
+    </div>
   )
 }
 
