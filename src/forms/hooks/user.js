d5b5a003617163ae8d69552e9e00d552e4bdd9b6
José Leonardo <joseleonaordo85@gnail.com>
2025-06-16 08:17:56 -0300

    refactor: add sign up form hook

--- src/forms/hooks/user.js
+++ src/forms/hooks/user.js
@@ -1,7 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 
-import { loginFormSchema } from '../schemas/user'
+import { loginFormSchema, signupFormSchema } from '../schemas/user'
 
 export const useLoginForm = () => {
   const form = useForm({
@@ -9,6 +9,22 @@ export const useLoginForm = () => {
     defaultValues: {
       email: '',
       password: '',
+    },
+  })
+
+  return { form }
+}
+
+export const useSignupForm = () => {
+  const form = useForm({
+    resolver: zodResolver(signupFormSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      terms: false,
     },
   })
 
--- src/forms/schemas/user.js
+++ src/forms/schemas/user.js
@@ -11,3 +11,31 @@ export const loginFormSchema = z.object({
     .trim()
     .min(6, { message: 'O campo senha deve conter no mínimo 6 caracteres' }),
 })
+
+export const signupFormSchema = z
+  .object({
+    firstName: z.string().trim().min(1, { message: 'O nome é obrigatório' }),
+    lastName: z
+      .string()
+      .trim()
+      .min(1, { message: 'O sobrenome é obrigatório' }),
+    email: z
+      .string()
+      .email({ message: 'O e-mail é inválido' })
+      .trim()
+      .min(1, { message: 'O e-mail é obrigatório' }),
+    password: z
+      .string()
+      .trim()
+      .min(6, { message: 'A senha deve ter no mínimo 6 caracteres' }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: 'A confirmação de senha deve ter no mínimo 6 caracteres',
+    }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Você precisa aceitar os termos',
+    }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'As senha não coincidem',
+    path: ['passwordConfirmation'],
+  })
